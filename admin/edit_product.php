9968562a31b3712672a65567ec9f1e8d92c90154
palmyudo <119488598+palmyudo@users.noreply.github.com>
2023-02-03 18:27:56 +0700

    edit_product

--- admin/edit_product.php
+++ admin/edit_product.php
@@ -1,60 +1,93 @@
 <?php
 session_start();
 require_once('../db.php');
-if(checkpermis()!=0)
-    {
-        go("../default.php");
-    }
-    if (isset($_POST['product_id']) && trim($_POST['product_id']) != ""){
-        $pid = $_POST['product_id'];
-        $pn = $_POST['product_name'];
-        // echo $pd;
-    $sql = "SELECT * FROM productss WHERE product_id='" . $pid."'";
-    // echo "$sql";
-    // echo "<br>";
-    $result = $conn->query($sql);
-    $rs = $result->fetch_array();
-    // $conn ขอการเชื่อมต่อ querry สั่งทำงาน
-    // echo $rs;
-     if ($result->num_rows == 1){
-        $sql = "UPDATE productss  SET product_name =('" . trim($_POST['product_name']) . "')WHERE product_id=$pid";
-
-    }
-    else{
-        alert("ไม่มีสินค้าตัวนี้");
-
-    } ?>
-    <!DOCTYPE html>
-<html lang="en">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="font/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-</head>
-<body class="text-center">
-        <div class="container">
-        <?php
-    $pid = $_GET['product_id'];
-    $sql = "SELECT * FROM productss WHERE product_id = $pid";
-    $result = $conn->query($sql);
-    $rs = $result->fetch_array();
-    $pid = $rs['product_id'];
-    $pn = $rs['product_name'];
-    ?>
-        <input for="product_name" name="product_name" placeholder="กรุณาเลือกสินค้าที่ต้องการแก้ไข " value="<?=$pn?>">
-                    <input type="hidden" name="product_id" value="<?=$pid?>">
-                    <br>
-                    <button type="submit" class="btn btn-outline-light me-2" value="OK!"><i class="fa fa-paper-plane-o" aria-hidden="true"></i>submit!</button>
-
-        </div>
-</body>
-</html>
-    <?php
-    } 
-else{
-    go("../index.php");
+if (checkpermis() != 0) {
+    go("../default.php");
 }
-    ?>
+if (isset($_GET['product_id']) && trim($_GET['product_id']) != "") {
 
+
+
+    if (isset($_POST['product_id']) && trim($_POST['product_id']) != "") {
+        $pid = $_POST['product_id'];
+        $pn = trim($_POST['product_name']);
+        $pp = $_POST['product_price'];
+        $cid = $_POST['cate_id'];
+        $pqty = $_POST['product_qty'];
+        // echo $pqty;
+        $sql = "SELECT * FROM productss WHERE product_id='" . $pid . "'";
+        // echo "$sql";
+        // echo "<br>";
+        $result = $conn->query($sql);
+        $rs = $result->fetch_array();
+        // $conn ขอการเชื่อมต่อ querry สั่งทำงาน
+        // echo $rs;
+        if ($result->num_rows == 1) {
+            $sql = "UPDATE productss  SET cate_id = '" . $cid . "',product_name = '" . $pn . "',product_price = '" . $pp . "',product_qty = '" . $pqty . "' WHERE product_id=$pid";
+            $result = $conn->query($sql);
+            // echo "$sql";
+            
+        } else {
+            alert("ไม่มีสินค้าตัวนี้");
+        }
+    } ?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link rel="stylesheet" href="font/css/font-awesome.min.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        </head>
+
+        <body class="text-center">
+            <div class="container">
+                <?php
+                $pid = $_GET['product_id'];
+                $sql = "SELECT * FROM productss WHERE product_id = $pid";
+                $result = $conn->query($sql);
+                $rs = $result->fetch_array();
+                $pid = $rs['product_id'];
+                $pn = $rs['product_name'];
+                $pp = $rs['product_price'];
+                $pqty = $rs['product_qty'];
+
+                ?>
+                <form class="form " action="" method="POST">
+                    <div class="form-inline">
+                        <label for="cate_id">ชื่อของสินค้า</label>
+                        <input type="text" name="product_name" placeholder="ชื่อของสินค้า" value="<?= $pn ?>" required>
+                        <select name="cate_id" id="cate_id">
+                            <?php
+                            $sqlz = "SELECT * FROM categorys ORDER BY cate_name";
+                            $resulti = $conn->query($sqlz);
+                            while ($rsi = $resulti->fetch_array()) {
+                                echo "<option value='" . $rsi['cate_id'] . "' >" . $rsi['cate_name'] . "</option>";
+                            }
+                            ?>
+                        </select>
+                        <input type="number" name="product_price" placeholder="ราคา" value="<?= $pp ?>" min="1" require>
+                        <input type="number" name="product_qty" placeholder="จำนวน" value="<?= $pqty ?>" min="1" require>
+                        <input type="hidden" name="product_id" value="<?= $pid ?>">
+                    </div>
+                    <br>
+                    <button class="w-50 btn btn-lg btn-outline-primary" type="submit" value="ok">Add</button>
+                    <br><br>
+                    <a class="w-10 btn btn-lg btn-primary" href="../admin.php">Close</a>
+                    <br>
+                </form>
+            </div>
+       
+        </body>
+
+        </html>
+<?php
+    
+}
+else {
+    go("../admin.php");
+}
+?>
--- sigup.php
+++ sigup.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/signin.css">
 </head>
 <body>
     <form action="sigupv0.php" method="post">
