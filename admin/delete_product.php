4833c5a179e36be6c9404cd3800ce7d3f253008c
palmyudo <119488598+palmyudo@users.noreply.github.com>
2023-02-04 17:10:22 +0700

    delete product ได้แล้ว

--- admin/delete_product.php
+++ admin/delete_product.php
@@ -1,20 +1,20 @@
 <?php
 session_start();
-if (isset($_SESSION['user_level'])  == 0) { 
+if (isset($_SESSION['user_level']) && $_SESSION['user_level'] == 0) { 
     require_once('../db.php');
     if (isset($_GET['product_id'])) {
-        $id = $_GET['product_id'];
-        $sql = "DELETE FROM productss WHERE product_id='$id'";
+        $pid = $_GET['product_id'];
+        $sql = "DELETE FROM productss WHERE product_id='$pid'";
         if ($result = $conn->query($sql) == true) {
             header('location:../admin.php');
         } else {
             header('location:../admin.php');
         }
     } else {
-        header('location: ../admin.php');
+        header('location:../admin.php');
     }
 } else {
-    header('location: ../index.php');
+    header('location:../index.php');
 
 }
 
